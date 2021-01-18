498c4ff444b1319b2d6d05ab18db9247439d016b
NathanReynoso <sourlemonz111@gmail.com>
2021-01-18 07:44:59 +0000

    Finished refactor_string_processing

--- refactor_string_processing.py
+++ refactor_string_processing.py
@@ -1,16 +1,18 @@
 string = '"alpha","beta","gamma","delta"\n1,2,3,4\n5.0,6.0,7.0,8.0'
-strings = [x.split(',') for x in string.split('\n')]
-arr = []
+lists = string.split('\n')
+strings = [x.split(',') for x in lists]
+
+output_arr = []
 for string in strings:
-    newstring = []
-    if len(string) > 0:
-        for char in string:
-            if char[0] == '""' and char[-1] == '""':
-                char = char[1:]
-            elif '.' in char:
-                char = int(char)
-            else:
-                char = float(char)
-            newstring.append(char)
-        arr.append(newstring)
-print(arr)
+  newstring = []
+  if len(string) > 0:
+    for char in string:
+      if char[0]=='"' and char[-1]=='"':
+        char = char[1:-1]
+      elif '.' in char:
+        char = float(char)
+      else:
+        char = int(char)
+      newstring.append(char)
+    output_arr.append(newstring)
+print(output_arr)
