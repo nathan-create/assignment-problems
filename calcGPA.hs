14ef95a0660f13201d9b9a07fdb8457ce7efdf4b
NathanReynoso <sourlemonz111@gmail.com>
2021-03-05 01:14:56 +0000

    Completed calcGPA for test 2-3

--- calcGPA.hs
+++ calcGPA.hs
@@ -1,1 +1,9 @@
-calcGPA x = 
+gradeToNum ['A'] = 4
+gradeToNum ['B'] = 3
+gradeToNum ['C'] = 2
+gradeToNum ['D'] = 1
+gradeToNum ['F'] = 0
+
+calcGPA grades = fromIntegral(sum(map(gradeToNum) grades))/fromIntegral(length(grades))
+
+main = print(calcGPA ["A", "B", "B", "C", "C", "C", "D", "F"]) 
